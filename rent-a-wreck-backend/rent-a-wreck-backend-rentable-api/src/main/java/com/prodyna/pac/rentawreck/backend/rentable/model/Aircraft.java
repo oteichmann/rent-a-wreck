e415f6c0ffcfa6f6a6e9e8246dffa067d0f30873
Oliver Teichmann <oteichmann@prodyna.com>
2014-06-04 22:35:11 +0200

    Changed impl to use abstract class for CRUD.

--- rent-a-wreck-backend/rent-a-wreck-backend-rentable-api/src/main/java/com/prodyna/pac/rentawreck/backend/rentable/model/Aircraft.java
+++ rent-a-wreck-backend/rent-a-wreck-backend-rentable-api/src/main/java/com/prodyna/pac/rentawreck/backend/rentable/model/Aircraft.java
@@ -2,15 +2,41 @@ package com.prodyna.pac.rentawreck.backend.rentable.model;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import com.prodyna.pac.rentawreck.backend.common.model.AbstractEntity;
 
 /**
+ * Aircraft 
+ * 
  * @author oteichmann
  * 
  */
 @Entity
 @Table(name = "raw_aircrafts")
-public class Aircraft extends Rentable {
+public class Aircraft extends AbstractEntity {
 
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = -6523878611712737965L;
+
+	@NotNull
+	private String id;
+	@NotNull
+	private AircraftType type;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public AircraftType getType() {
+		return type;
+	}
+
+	public void setType(AircraftType type) {
+		this.type = type;
+	}
 
 }
--- rent-a-wreck-backend/rent-a-wreck-backend-rentable-api/src/main/java/com/prodyna/pac/rentawreck/backend/rentable/service/AircraftService.java
+++ rent-a-wreck-backend/rent-a-wreck-backend-rentable-api/src/main/java/com/prodyna/pac/rentawreck/backend/rentable/service/AircraftService.java
@@ -6,50 +6,19 @@ package com.prodyna.pac.rentawreck.backend.rentable.service;
 import java.util.List;
 
 import javax.annotation.security.PermitAll;
-import javax.annotation.security.RolesAllowed;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import org.jboss.ejb3.annotation.SecurityDomain;
-
+import com.prodyna.pac.rentawreck.backend.common.service.AbstractEntityPersistenceService;
 import com.prodyna.pac.rentawreck.backend.rentable.model.Aircraft;
 
 /**
  * @author oteichmann
  */
 @Path("/aircrafts")
-@SecurityDomain("rent-a-wreck")
-public interface AircraftService {
-
-	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-	@RolesAllowed("admin")
-	public Aircraft create(Aircraft aircraft);
-
-	@PUT
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-	@RolesAllowed({"admin"})
-	public Aircraft update(Aircraft aircraft);
-
-	@DELETE
-	@Path("/{uuid}")
-	@RolesAllowed("admin")
-	public void delete(@PathParam("uuid") String uuid);
-
-	@GET
-	@Path("/{uuid}")
-	@Produces(MediaType.APPLICATION_JSON)
-	@PermitAll
-	public Aircraft findById(@PathParam("uuid") String uuid);
+public interface AircraftService extends AbstractEntityPersistenceService<Aircraft> {
 
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
