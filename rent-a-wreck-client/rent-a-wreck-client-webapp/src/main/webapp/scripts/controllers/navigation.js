d2b59594e857f7e88fbd5afc63218f3c66ff2003
Oliver Teichmann <oteichmann@prodyna.com>
2014-06-18 18:14:58 +0200

    code formatting

--- rent-a-wreck-client/rent-a-wreck-client-webapp/src/main/webapp/scripts/controllers/navigation.js
+++ rent-a-wreck-client/rent-a-wreck-client-webapp/src/main/webapp/scripts/controllers/navigation.js
@@ -7,28 +7,26 @@ rawControllers.controller('navigationCtrl', function($rootScope, $scope, $cookie
 	/**
 	 * Validate authentication cookie on load
 	 */
-//	$rootScope.$on('$routeChangeSuccess', function ($angularEvent, $current, $previous) {
-		var token = $cookies["XSRF-TOKEN"];
-		if (token) {
-			$http.post("/rent-a-wreck-rest/auth/validate", JSON.stringify({
-				token : token
-			})).success(function(data, status, headers, config) {
-				// TODO: return already logged in message, get user data, ...
-				//return $http.get("/users/" + response.data.userId);
-				$rootScope.loggedIn = true;
-			}).error(function(data, status, headers, config) {
-				// Invalidate all existing cookies if token was not valid
-				$rootScope.loggedIn = false;
-//				$cookies["XSRF-TOKEN"] = undefined;
-			});
-		} else {
-			$scope.loggedIn = false;
-		}
-//    });
-
-		$rootScope.$on('$routeChangeSuccess', function ($angularEvent, $current, $previous) {
-			// TODO: check if user is permissioned
+	var token = $cookies["XSRF-TOKEN"];
+	if (token) {
+		$http.post("/rent-a-wreck-rest/auth/validate", JSON.stringify({
+			token : token
+		})).success(function(data, status, headers, config) {
+			// TODO: return already logged in message, get user data, ...
+			//return $http.get("/users/" + response.data.userId);
+			$rootScope.loggedIn = true;
+		}).error(function(data, status, headers, config) {
+			// Invalidate all existing cookies if token was not valid
+			$rootScope.loggedIn = false;
+			// $cookies["XSRF-TOKEN"] = undefined;
 		});
+	} else {
+		$scope.loggedIn = false;
+	}
+
+	$rootScope.$on('$routeChangeSuccess', function ($angularEvent, $current, $previous) {
+		// TODO: check if user is permissioned
+	});
 		
 	/**
 	 * Invalidate the token on the server.
